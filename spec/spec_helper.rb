06645456629443bb2308b82e810e6ce84e6e6dd3
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-05 14:15:53 +0100

    Configure RSpec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,13 @@
-# frozen_string_literal: true
-
 require "kangaru"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  Kernel.srand(config.seed)
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.default_formatter = :doc if config.files_to_run.one?
+
   config.disable_monkey_patching!
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
+
+  config.order = :random
 end
