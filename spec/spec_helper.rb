4ddf215530fb531e9b3cd8952d252c1642097029
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-10 13:45:56 +0100

    Autorequire spec support files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require "kangaru"
 
+def load_support_files!
+  Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+end
+
 RSpec.configure do |config|
+  load_support_files!
+
   Kernel.srand(config.seed)
 
   config.default_formatter = :doc if config.files_to_run.one?
