8b8eddf2832b4a7b3f2c4ddf455857bae76dddeb
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-15 21:17:28 +0100

    Implement inflector token joining

--- lib/kangaru/inflectors/inflector.rb
+++ lib/kangaru/inflectors/inflector.rb
@@ -9,7 +9,9 @@ module Kangaru
 
       def inflect
         tokeniser.split.map do |tokens|
-          tokens.map { |token| transform_token(token) }.join
+          tokens.map! { |token| transform_token(token) }
+
+          join_tokens(tokens)
         end.join
       end
 
@@ -23,12 +25,20 @@ module Kangaru
         class_attribute(:token_transformer)
       end
 
+      def token_joiner
+        class_attribute(:token_joiner)
+      end
+
       def transform_token(token)
         case token_transformer
         when Proc   then token_transformer.call(token)
         when Symbol then token.send(token_transformer)
         else token
         end
+      end
+
+      def join_tokens(tokens)
+        tokens.join(token_joiner)
       end
     end
   end
