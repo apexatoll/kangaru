a5e5f9859fbc1e78f6ab0ef088552103cff0d76c
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-18 00:22:10 +0100

    Implement input wrapper

---
+++ lib/kangaru/input.rb
@@ -0,0 +1,19 @@
+module Kangaru
+  class Input
+    attr_reader :controller, :action, :arguments
+
+    def initialize(controller:, action:, arguments:)
+      @controller = controller
+      @action = action
+      @arguments = arguments
+    end
+
+    def self.from_argv(tokens)
+      controller = InputParsers::ControllerParser.parse(tokens)
+      action     = InputParsers::ActionParser.parse(tokens)
+      arguments  = InputParsers::ArgumentParser.parse(tokens)
+
+      new(controller:, action:, arguments:)
+    end
+  end
+end
