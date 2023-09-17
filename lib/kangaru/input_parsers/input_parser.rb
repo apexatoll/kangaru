4665a8c2a2496ecf658005edf09475a3f0e94a15
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-17 22:36:10 +0100

    Implement token splitting methods

--- lib/kangaru/input_parsers/input_parser.rb
+++ lib/kangaru/input_parsers/input_parser.rb
@@ -1,6 +1,8 @@
 module Kangaru
   module InputParsers
     class InputParser
+      ARGUMENT_TOKEN = /^-/
+
       attr_reader :tokens
 
       def initialize(tokens)
@@ -9,6 +11,14 @@ module Kangaru
 
       def parse
         raise NotImplementedError
+      end
+
+      def command_tokens
+        tokens.take_while { |token| !token.match?(ARGUMENT_TOKEN) }
+      end
+
+      def argument_tokens
+        tokens.drop_while { |token| !token.match?(ARGUMENT_TOKEN) }
       end
     end
   end
