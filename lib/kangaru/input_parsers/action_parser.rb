5f8c2321cd8f4cc5b36ac57896edbc53e4a9e045
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-17 23:14:39 +0100

    Implement action parser

---
+++ lib/kangaru/input_parsers/action_parser.rb
@@ -0,0 +1,17 @@
+module Kangaru
+  module InputParsers
+    class ActionParser < InputParser
+      using Patches::Inflections
+
+      DEFAULT_ACTION = :default
+
+      def parse
+        if command_tokens.count > 1
+          command_tokens[-1].to_snakecase.to_sym
+        else
+          DEFAULT_ACTION
+        end
+      end
+    end
+  end
+end
