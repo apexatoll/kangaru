947027e8b12651c524f78011b043aef7af0d077b
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-18 00:07:20 +0100

    Implement argument parser

---
+++ lib/kangaru/input_parsers/argument_parser.rb
@@ -0,0 +1,30 @@
+module Kangaru
+  module InputParsers
+    class ArgumentParser < InputParser
+      using Patches::Inflections
+
+      def parse
+        grouped_argument_tokens.to_h do |tokens|
+          key   = parse_key(tokens.shift || raise)
+          value = parse_value(tokens)
+
+          [key, value]
+        end
+      end
+
+      private
+
+      def grouped_argument_tokens
+        argument_tokens.slice_before(ARGUMENT_TOKEN).to_a
+      end
+
+      def parse_key(key)
+        key.gsub(/^-+/, "").to_snakecase.to_sym
+      end
+
+      def parse_value(tokens)
+        tokens.empty? || tokens.join(" ")
+      end
+    end
+  end
+end
