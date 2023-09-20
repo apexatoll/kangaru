6ff53e5bb6134c513028a7a870b1d080477ef40a
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-20 20:51:59 +0100

    Implement application autoloading

--- lib/kangaru/application.rb
+++ lib/kangaru/application.rb
@@ -13,5 +13,18 @@ module Kangaru
     def app_dir
       @app_dir ||= File.join(root_dir, namespace.to_s.to_snakecase)
     end
+
+    def setup
+      autoloader.setup
+    end
+
+    private
+
+    def autoloader
+      @autoloader ||= Zeitwerk::Loader.new.tap do |loader|
+        loader.inflector = Zeitwerk::GemInflector.new(root_file)
+        loader.push_dir(app_dir)
+      end
+    end
   end
 end
--- spec/kangaru/application_spec.rb
+++ spec/kangaru/application_spec.rb
@@ -18,4 +18,41 @@ RSpec.describe Kangaru::Application do
       expect(app_dir).to eq("/some_app/lib/some_app")
     end
   end
+
+  describe "#setup" do
+    subject(:setup) { application.setup }
+
+    before do
+      allow(Zeitwerk::Loader).to receive(:new).and_return(loader)
+      allow(Zeitwerk::GemInflector).to receive(:new).and_return(gem_inflector)
+    end
+
+    let(:loader) { instance_spy(Zeitwerk::Loader) }
+    let(:gem_inflector) { instance_spy(Zeitwerk::GemInflector) }
+
+    it "instantiates a Zeitwerk loader" do
+      setup
+      expect(Zeitwerk::Loader).to have_received(:new).once
+    end
+
+    it "instantiates a Zeitwerk gem inflector for the root file" do
+      setup
+      expect(Zeitwerk::GemInflector).to have_received(:new).with(root_file).once
+    end
+
+    it "configures the loader to use the instantiated gem inflector" do
+      setup
+      expect(loader).to have_received(:inflector=).with(gem_inflector).once
+    end
+
+    it "configures the loader to load the app dir" do
+      setup
+      expect(loader).to have_received(:push_dir).with(application.app_dir).once
+    end
+
+    it "enables the instantiated loader" do
+      setup
+      expect(loader).to have_received(:setup).once
+    end
+  end
 end
