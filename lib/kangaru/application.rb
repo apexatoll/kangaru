112d9bb649f91baad9db0147cf0b5dbc3b3495be
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-20 20:31:04 +0100

    Implement Application#app_dir

--- lib/kangaru/application.rb
+++ lib/kangaru/application.rb
@@ -1,11 +1,17 @@
 module Kangaru
   class Application
+    using Patches::Inflections
+
     attr_reader :root_file, :root_dir, :namespace
 
     def initialize(root_file:, namespace:)
       @root_file = root_file
       @root_dir  = File.dirname(root_file)
       @namespace = namespace
+    end
+
+    def app_dir
+      @app_dir ||= File.join(root_dir, namespace.to_s.to_snakecase)
     end
   end
 end
