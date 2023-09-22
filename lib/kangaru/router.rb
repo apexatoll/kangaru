07a14819913cd9c9bc831c0c7a96e2499a3a50ab
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-22 18:52:02 +0100

    Implement router validations

--- lib/kangaru/router.rb
+++ lib/kangaru/router.rb
@@ -2,11 +2,38 @@ module Kangaru
   class Router
     using Patches::Constantise
 
+    class UndefinedControllerError < StandardError; end
+
+    class UndefinedActionError < StandardError; end
+
     attr_reader :command, :namespace
 
     def initialize(command, namespace: Object)
       @command   = command
       @namespace = namespace
+
+      validate_controller_defined!
+      validate_action_defined!
+    end
+
+    private
+
+    def controller_class
+      @controller_class ||= command.controller.constantise(root: namespace)
+    end
+
+    def validate_controller_defined!
+      return if namespace.const_defined?(command.controller)
+
+      raise UndefinedControllerError,
+            "#{command.controller} is not defined in #{namespace}"
+    end
+
+    def validate_action_defined!
+      return if controller_class.instance_methods.include?(command.action)
+
+      raise UndefinedActionError,
+            "#{command.action} is not defined by #{command.controller}"
     end
   end
 end
--- spec/kangaru/router_spec.rb
+++ spec/kangaru/router_spec.rb
@@ -9,11 +9,50 @@ RSpec.describe Kangaru::Router do
 
   let(:namespace) { SomeNamespace }
 
-  before { stub_const "SomeNamespace", Module.new }
+  let(:controller_class) do
+    Class.new(described_class) { def some_action = nil }
+  end
+
+  before do
+    stub_const "SomeNamespace", Module.new
+    stub_const "SomeNamespace::SomeController", controller_class
+  end
 
   describe "#initialize" do
-    it "sets the attributes" do
-      expect(router).to have_attributes(command:, namespace:)
+    context "when command controller is not defined" do
+      let(:controller) { "AnotherController" }
+
+      it "raises an error" do
+        expect { router }.to raise_error(
+          described_class::UndefinedControllerError,
+          "#{controller} is not defined in #{namespace}"
+        )
+      end
+    end
+
+    context "when command controller is defined" do
+      let(:controller) { "SomeController" }
+
+      context "and command action is not defined" do
+        let(:controller_class) { Class.new(described_class) }
+
+        it "raises an error" do
+          expect { router }.to raise_error(
+            described_class::UndefinedActionError,
+            "#{action} is not defined by #{controller}"
+          )
+        end
+      end
+
+      context "and command action is defined" do
+        it "does not raise an error" do
+          expect { router }.not_to raise_error
+        end
+
+        it "sets the attributes" do
+          expect(router).to have_attributes(command:, namespace:)
+        end
+      end
     end
   end
 end
