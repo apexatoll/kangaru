8c99423c081a60eea64faee7e728def56bbe85f0
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-25 00:22:09 +0100

    Implement application paths module

---
+++ lib/kangaru/application_paths.rb
@@ -0,0 +1,23 @@
+module Kangaru
+  module ApplicationPaths
+    def path
+      @path ||= Pathname.new(File.join(dir, name))
+    end
+
+    def lib_path
+      @lib_path ||= Pathname.new(File.join(path.to_s, "lib"))
+    end
+
+    def main_file
+      @main_file ||= Pathname.new(File.join(lib_path.to_s, "#{name}.rb"))
+    end
+
+    def gem_file(file)
+      Pathname.new(File.join(lib_path.to_s, name, "#{file}.rb"))
+    end
+
+    def gem_dir(dir)
+      Pathname.new(File.join(lib_path.to_s, name, dir))
+    end
+  end
+end
