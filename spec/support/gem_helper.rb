3f6e063a6b607b72acd08bb3cbea72f8c31f2083
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-24 18:01:33 +0100

    Add spec helper to generate gems for integration testing

---
+++ spec/support/gem_helper.rb
@@ -0,0 +1,21 @@
+require "tmpdir"
+
+module GemHelper
+  RSpec.configure do |config|
+    config.around(with_gem: true) do |spec|
+      self.class.class_eval { attr_reader :gem }
+
+      Dir.mktmpdir do |dir|
+        @gem = Kangaru::Testing::Gem.new(dir:).tap(&:create!)
+
+        spec.run
+
+        Object.send(:remove_const, :SomeGem) if Object.const_defined?(:SomeGem)
+
+        if Kangaru.instance_variable_defined?(:@application)
+          Kangaru.remove_instance_variable(:@application)
+        end
+      end
+    end
+  end
+end
