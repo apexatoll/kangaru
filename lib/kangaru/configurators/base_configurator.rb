0a8c65fa5ff0ded7e55bd11db83ac4ae17199c29
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-28 19:19:20 +0100

    Implement configurator name

--- lib/kangaru/configurators/base_configurator.rb
+++ lib/kangaru/configurators/base_configurator.rb
@@ -1,6 +1,15 @@
 module Kangaru
   module Configurators
     class BaseConfigurator
+      using Patches::Inflections
+
+      def self.name
+        to_s.gsub(/^.*::(?!.*::)/, "")
+            .delete_suffix("Configurator")
+            .to_snakecase
+            .to_sym
+      end
+
       def self.settings
         @settings ||= instance_methods.grep(/\w=$/).map do |setting|
           setting.to_s.delete_suffix("=").to_sym
