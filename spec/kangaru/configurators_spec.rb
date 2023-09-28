0d4d9f0566c59a13a846ba7e329d81cf2fd7c1c1
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-28 22:31:31 +0100

    Fix configurators spec

--- spec/kangaru/configurators_spec.rb
+++ spec/kangaru/configurators_spec.rb
@@ -3,12 +3,24 @@ RSpec.describe Kangaru::Configurators do
     subject(:classes) { described_class.classes }
 
     context "when no configurator classes are defined" do
+      before do
+        allow(described_class)
+          .to receive(:constants)
+          .and_return([])
+      end
+
       it "returns an empty array" do
         expect(classes).to be_empty
       end
     end
 
     context "when configurator classes are defined" do
+      before do
+        allow(described_class)
+          .to receive(:constants)
+          .and_return(%i[SomeConfigurator AnotherConfigurator])
+      end
+
       around do |spec|
         described_class.const_set(:SomeConfigurator, some_configurator)
         described_class.const_set(:AnotherConfigurator, another_configurator)
