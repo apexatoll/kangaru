0796c73f86de0d5e37f4262f250b868fa3891752
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-09-28 18:42:20 +0100

    Implement Configurator.classes

---
+++ spec/kangaru/configurators_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe Kangaru::Configurators do
+  describe ".classes" do
+    subject(:classes) { described_class.classes }
+
+    context "when no configurator classes are defined" do
+      it "returns an empty array" do
+        expect(classes).to be_empty
+      end
+    end
+
+    context "when configurator classes are defined" do
+      around do |spec|
+        described_class.const_set(:SomeConfigurator, some_configurator)
+        described_class.const_set(:AnotherConfigurator, another_configurator)
+        spec.run
+        described_class.send(:remove_const, :SomeConfigurator)
+        described_class.send(:remove_const, :AnotherConfigurator)
+      end
+
+      let(:some_configurator)    { Class.new }
+      let(:another_configurator) { Class.new }
+
+      it "returns the expected classes" do
+        expect(classes).to contain_exactly(
+          some_configurator, another_configurator
+        )
+      end
+    end
+  end
+end
