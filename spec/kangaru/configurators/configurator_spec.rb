8c128a3b24438d81aaff9e9270f6cbfe4509379e
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-01 23:12:07 +0100

    Refactor configurator to use attributes concern

--- spec/kangaru/configurators/configurator_spec.rb
+++ spec/kangaru/configurators/configurator_spec.rb
@@ -13,32 +13,6 @@ RSpec.describe Kangaru::Configurators::Configurator do
     end
   end
 
-  describe ".settings" do
-    subject(:settings) { configurator_class.settings }
-
-    context "when no attr_accessors are set" do
-      let(:configurator_class) do
-        Class.new(described_class)
-      end
-
-      it "returns an empty array" do
-        expect(settings).to be_empty
-      end
-    end
-
-    context "when attr_accessors are set" do
-      let(:configurator_class) do
-        Class.new(described_class) do
-          attr_accessor :foo, :bar, :baz
-        end
-      end
-
-      it "returns the expected settings" do
-        expect(settings).to contain_exactly(:foo, :bar, :baz)
-      end
-    end
-  end
-
   describe "#serialise" do
     subject(:hash) { configurator.serialise }
 
