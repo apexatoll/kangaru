5a8895acafe903471317b254f6329f2b822e6b56
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-18 00:33:45 +0100

    Add migration tests to database setup spec

--- spec/features/database_setup_spec.rb
+++ spec/features/database_setup_spec.rb
@@ -69,28 +69,140 @@ RSpec.describe "Setting up a database in a target gem", with_gem: :some_gem do
     end
 
     context "and database path is set" do
-      before do
-        gem.main_file.write(<<~RUBY)
-          require "kangaru"
+      context "and migration path is not set" do
+        before do
+          gem.main_file.write(<<~RUBY)
+            require "kangaru"
 
-          module SomeGem
-            extend Kangaru::Initialiser
+            module SomeGem
+              extend Kangaru::Initialiser
 
-            configure do |config|
-              config.database.adaptor = :sqlite
-              config.database.path    = "#{gem.path.join('database.sqlite3')}"
+              configure do |config|
+                config.database.adaptor = :sqlite
+                config.database.path    = "#{gem.path.join('database.sqlite3')}"
+              end
+            end
+          RUBY
+        end
+
+        it "does not raise any errors" do
+          expect { gem.load! }.not_to raise_error
+        end
+
+        it "sets the database instance" do
+          gem.load!
+          expect(Kangaru.application.database).to be_a(Kangaru::Database)
+        end
+
+        it "does not apply any migrations" do
+          gem.load!
+          expect(Kangaru.application.database.handler.tables).to be_empty
+        end
+      end
+
+      context "and migration path is set" do
+        let(:migration_path) { gem.gem_path("db", "migrate") }
+
+        before do
+          gem.gem_path("db").mkdir
+
+          gem.main_file.write(<<~RUBY)
+            require "kangaru"
+
+            module SomeGem
+              extend Kangaru::Initialiser
+
+              configure do |config|
+                config.database.adaptor = :sqlite
+                config.database.path = "#{gem.path.join('database.sqlite3')}"
+                config.database.migration_path = "#{migration_path}"
+              end
+            end
+          RUBY
+        end
+
+        context "and directory does not exist" do
+          it "does not raise any errors" do
+            expect { gem.load! }.not_to raise_error
+          end
+
+          it "sets the database instance" do
+            gem.load!
+            expect(Kangaru.application.database).to be_a(Kangaru::Database)
+          end
+
+          it "does not apply any migrations" do
+            gem.load!
+            expect(Kangaru.application.database.handler.tables).to be_empty
+          end
+        end
+
+        context "and directory exists" do
+          before do
+            migration_path.mkdir
+          end
+
+          context "and no migrations are present" do
+            it "does not raise any errors" do
+              expect { gem.load! }.not_to raise_error
+            end
+
+            it "sets the database instance" do
+              gem.load!
+              expect(Kangaru.application.database).to be_a(Kangaru::Database)
+            end
+
+            it "does not apply any migrations" do
+              gem.load!
+              expect(Kangaru.application.database.handler.tables).to be_empty
             end
           end
-        RUBY
-      end
 
-      it "does not raise any errors" do
-        expect { gem.load! }.not_to raise_error
-      end
+          context "and migrations are present" do
+            before do
+              migration_path.join("1_migration_one.rb").write(migration_one)
+              migration_path.join("2_migration_two.rb").write(migration_two)
+            end
 
-      it "sets the database instance" do
-        gem.load!
-        expect(Kangaru.application.database).to be_a(Kangaru::Database)
+            let(:migration_one) do
+              <<~RUBY
+                Sequel.migration do
+                  change do
+                    create_table :foos do
+                      primary_key :id
+                    end
+                  end
+                end
+              RUBY
+            end
+
+            let(:migration_two) do
+              <<~RUBY
+                Sequel.migration do
+                  change do
+                    create_table :bars do
+                      primary_key :id
+                    end
+                  end
+                end
+              RUBY
+            end
+
+            it "does not raise any errors" do
+              expect { gem.load! }.not_to raise_error
+            end
+
+            it "sets the database instance" do
+              gem.load!
+              expect(Kangaru.application.database).to be_a(Kangaru::Database)
+            end
+
+            it "applies the migrations" do
+              gem.load!
+              expect(Kangaru.application.database.handler.tables).to include(:foos, :bars)
+            end
+          end
+        end
       end
     end
   end
