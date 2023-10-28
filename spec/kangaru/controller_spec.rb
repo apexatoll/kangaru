76fb2dd66bafb631d713e9cde777ef6e9afb2f49
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-28 16:42:42 +0100

    Ensure renderer binding is scoped to the application namespace
    The binding passed to the renderer is not scoped to the application
    namespace, and as such, all application classes must be prefixed with
    the namespace module.
    
    This change emulates the binding being created from within the
    application namespace by delegating const lookups to said namespace if
    the constant is not in scope from the Kangaru::Controller class.

--- spec/kangaru/controller_spec.rb
+++ spec/kangaru/controller_spec.rb
@@ -51,6 +51,54 @@ RSpec.describe Kangaru::Controller do
       expect(renderer).to have_received(:render).with(a_kind_of(Binding)).once
     end
   end
+
+  describe ".const_missing" do
+    subject(:lookup_const) { described_class::Foobar }
+
+    let(:foobar) { Module.new }
+
+    let(:application) do
+      instance_spy(Kangaru::Application, namespace: Namespace)
+    end
+
+    before do
+      stub_const "Namespace", Module.new
+
+      allow(Kangaru).to receive(:application).and_return(application)
+    end
+
+    context "when specified const is not defined inside Controller" do
+      context "and const not defined in application namespace" do
+        it "raises an error" do
+          expect { lookup_const }.to raise_error(NameError)
+        end
+      end
+
+      context "and const defined in application namespace" do
+        before { stub_const "Namespace::Foobar", foobar }
+
+        it "does not raise any errors" do
+          expect { lookup_const }.not_to raise_error
+        end
+
+        it "delegates the const lookup to the application namespace" do
+          expect(lookup_const).to eq(foobar)
+        end
+      end
+    end
+
+    context "when specified const is defined inside Controller" do
+      before { stub_const "#{described_class}::Foobar", foobar }
+
+      it "does not raise any errors" do
+        expect { lookup_const }.not_to raise_error
+      end
+
+      it "delegates the const lookup to the application namespace" do
+        expect(lookup_const).to eq(foobar)
+      end
+    end
+  end
 end
 
 # rubocop:enable RSpec/SubjectStub
