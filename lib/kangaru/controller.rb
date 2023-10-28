3742aba5e0efd79cfd504f2a343c4a91565af480
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-28 16:42:42 +0100

    Ensure renderer binding is scoped to the application namespace
    The binding passed to the renderer is not scoped to the application
    namespace, and as such, all application classes must be prefixed with
    the namespace module.
    
    This change emulates the binding being created from within the
    application namespace by delegating const lookups to said namespace if
    the constant is not in scope from the Kangaru::Controller class.

--- lib/kangaru/controller.rb
+++ lib/kangaru/controller.rb
@@ -12,5 +12,18 @@ module Kangaru
 
       renderer.render(binding)
     end
+
+    # The binding passed to the renderer is not scoped to the application
+    # namespace, and as such, all application classes must be prefixed with the
+    # namespace module. This change emulates the binding being created from
+    # within the application namespace by delegating const lookups to said
+    # namespace if the constant is not in scope from the current class.
+    def self.const_missing(const)
+      return super unless Kangaru.application.namespace.const_defined?(const)
+
+      Kangaru.application.namespace.const_get(const)
+    end
+
+    private_class_method :const_missing
   end
 end
