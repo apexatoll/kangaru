6ddb455acf0e8e49e2993f0596db7b628acf697c
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-10-31 02:14:09 +0000

    Implement Request#controller

--- lib/kangaru/request.rb
+++ lib/kangaru/request.rb
@@ -1,10 +1,28 @@
 module Kangaru
   class Request
+    using Patches::Inflections
+
+    DEFAULT_CONTROLLER = "DefaultController".freeze
+
+    CONTROLLER_SUFFIX = "Controller".freeze
+
     attr_reader :path, :params
 
     def initialize(path:, params:)
       @path = path
       @params = params
+    end
+
+    def controller
+      return DEFAULT_CONTROLLER if path_parser.controller.nil?
+
+      path_parser.controller&.to_class_name&.concat(CONTROLLER_SUFFIX) || raise
+    end
+
+    private
+
+    def path_parser
+      @path_parser ||= PathParser.new(path)
     end
   end
 end
--- spec/kangaru/request_spec.rb
+++ spec/kangaru/request_spec.rb
@@ -7,9 +7,45 @@ RSpec.describe Kangaru::Request do
 
   let(:params) { { foo: "foo", bar: "bar" } }
 
+  let(:path_parser) do
+    instance_spy(Kangaru::PathParser, controller: path_controller)
+  end
+
+  before do
+    allow(Kangaru::PathParser).to receive(:new).and_return(path_parser)
+  end
+
   describe "#initialize" do
     it "sets the attributes" do
       expect(request).to have_attributes(**attributes)
+    end
+  end
+
+  describe "#controller" do
+    subject(:controller) { request.controller }
+
+    context "when no controller is extracted from the path" do
+      let(:path_controller) { nil }
+
+      it "returns the default controller name" do
+        expect(controller).to eq(described_class::DEFAULT_CONTROLLER)
+      end
+    end
+
+    context "when base controller is extracted from the path" do
+      let(:path_controller) { "foobar" }
+
+      it "returns the expected controller name" do
+        expect(controller).to eq("FoobarController")
+      end
+    end
+
+    context "when namespaced controller is extracted from the path" do
+      let(:path_controller) { "foobar/baz_bar" }
+
+      it "returns the expected controller name" do
+        expect(controller).to eq("Foobar::BazBarController")
+      end
     end
   end
 end
