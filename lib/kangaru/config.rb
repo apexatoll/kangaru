7e83c7c0cc98c4c829d7a4082df91ebb2b235381
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-01 18:51:28 +0000

    Implement Config#for

--- lib/kangaru/config.rb
+++ lib/kangaru/config.rb
@@ -17,6 +17,13 @@ module Kangaru
                     .from_yaml_file(application.config_path)
     end
 
+    # Returns the configurator instance with the given class name.
+    def for(configurator_name)
+      configurators.values.find do |configurator|
+        configurator.class.name == configurator_name # rubocop:disable Style
+      end
+    end
+
     private
 
     def set_configurators!
