0b31fd61ec19f6366543402669f25a2096403efa
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-02 22:09:37 +0000

    Implement AttributeValidator#validate

--- lib/kangaru/validation/attribute_validator.rb
+++ lib/kangaru/validation/attribute_validator.rb
@@ -1,11 +1,31 @@
 module Kangaru
   module Validation
     class AttributeValidator
+      using Patches::Inflections
+
       attr_reader :model, :attribute
 
       def initialize(model:, attribute:)
         @model = model
         @attribute = attribute
+      end
+
+      def validate(validator, **params)
+        load_validator(validator:, params:).validate
+      end
+
+      private
+
+      def validator_name(validator)
+        "#{validator.to_s.to_class_name}Validator"
+      end
+
+      def load_validator(validator:, params:)
+        name = validator_name(validator)
+
+        raise "#{name} is not defined" unless Validators.const_defined?(name)
+
+        Validators.const_get(name).new(model:, attribute:, params:)
       end
     end
   end
--- spec/kangaru/validation/attribute_validator_spec.rb
+++ spec/kangaru/validation/attribute_validator_spec.rb
@@ -1,11 +1,67 @@
 RSpec.describe Kangaru::Validation::AttributeValidator do
   subject(:attribute_validator) { described_class.new(model:, attribute:) }
 
-  let(:model) { SomeModel }
+  let(:model) { SomeModel.new(some_attribute: model_value) }
 
   let(:attribute) { :some_attribute }
 
-  before { stub_const "SomeModel", Class.new }
+  let(:model_class) do
+    Class.new do
+      attr_accessor :some_attribute
 
-  pending :implmentation
+      def initialize(some_attribute:)
+        @some_attribute = some_attribute
+      end
+    end
+  end
+
+  let(:model_value) { :some_value }
+
+  before { stub_const "SomeModel", model_class }
+
+  describe "#validate" do
+    subject(:validate) { attribute_validator.validate(validator, **params) }
+
+    let(:validator) { :foobar }
+    let(:params) { {} }
+
+    shared_context :validator_defined do
+      before do
+        stub_const "Kangaru::Validators::FoobarValidator", validator_class
+
+        allow(Kangaru::Validators::FoobarValidator)
+          .to receive(:new)
+          .and_return(validator_instance)
+      end
+
+      let(:validator_instance) { instance_spy(validator_class) }
+
+      let(:validator_class) do
+        Class.new(Kangaru::Validators::Validator)
+      end
+    end
+
+    context "when inferred validator is not defined in validators namespace" do
+      it "raises an error" do
+        expect { validate }.to raise_error("FoobarValidator is not defined")
+      end
+    end
+
+    context "when inferred validator is defined in validators namespace" do
+      include_context :validator_defined
+
+      it "instantiates the inferred validator" do
+        validate
+
+        expect(Kangaru::Validators::FoobarValidator)
+          .to have_received(:new)
+          .with(model:, attribute:, params:)
+      end
+
+      it "runs the validator" do
+        validate
+        expect(validator_instance).to have_received(:validate).once
+      end
+    end
+  end
 end
