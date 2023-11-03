8499170847094587357569130878ced89693dabc
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-03 01:07:38 +0000

    Implement Validatable#validate

--- lib/kangaru/concerns/validatable.rb
+++ lib/kangaru/concerns/validatable.rb
@@ -3,8 +3,32 @@ module Kangaru
     module Validatable
       extend Concern
 
+      class_methods do
+        def validations
+          @validations ||= {}
+        end
+      end
+
       def errors
         @errors ||= []
+      end
+
+      def validate
+        self.class.validations.each do |attribute, validations|
+          validator = validator_for(attribute)
+
+          validations.each do |validator_name, params|
+            params = {} if params == true
+
+            validator.validate(validator_name, **params)
+          end
+        end
+      end
+
+      private
+
+      def validator_for(attribute)
+        Validation::AttributeValidator.new(model: self, attribute:)
       end
     end
   end
