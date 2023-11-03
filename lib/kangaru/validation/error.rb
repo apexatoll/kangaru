87d2405c6962e0012f127f7afb084a6c20091152
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-03 02:26:18 +0000

    Implement Error#full_message

--- lib/kangaru/validation/error.rb
+++ lib/kangaru/validation/error.rb
@@ -1,11 +1,29 @@
 module Kangaru
   module Validation
     class Error
+      using Patches::Inflections
+
+      MESSAGES = {}.freeze
+
       attr_reader :attribute, :type
 
       def initialize(attribute:, type:)
         @attribute = attribute
         @type = type
+      end
+
+      def full_message
+        [human_attribute, message].join(" ")
+      end
+
+      private
+
+      def human_attribute
+        attribute.to_s.to_humanised
+      end
+
+      def message
+        MESSAGES[type] || raise("invalid message type")
       end
     end
   end
--- spec/kangaru/validation/error_spec.rb
+++ spec/kangaru/validation/error_spec.rb
@@ -12,4 +12,37 @@ RSpec.describe Kangaru::Validation::Error do
       expect(error).to have_attributes(**attributes)
     end
   end
+
+  describe "#full_message" do
+    subject(:full_message) { error.full_message }
+
+    let(:messages) { { valid_validator => "is invalid" } }
+
+    let(:valid_validator)   { :invalid }
+    let(:invalid_validator) { :another }
+
+    before do
+      stub_const "#{described_class}::MESSAGES", messages
+    end
+
+    context "when message does not exist for given type" do
+      let(:type) { invalid_validator }
+
+      it "raises an error" do
+        expect { full_message }.to raise_error("invalid message type")
+      end
+    end
+
+    context "when message exists for given type" do
+      let(:type) { valid_validator }
+
+      it "does not raise any errors" do
+        expect { full_message }.not_to raise_error
+      end
+
+      it "returns the expected string" do
+        expect(full_message).to eq("Some attribute is invalid")
+      end
+    end
+  end
 end
