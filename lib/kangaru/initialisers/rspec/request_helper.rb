b0905de68bbb2b7964ef2e6cb744c1d0a894ad81
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-08 09:03:44 +0000

    Ensure request specs stub output

--- lib/kangaru/initialisers/rspec/request_helper.rb
+++ lib/kangaru/initialisers/rspec/request_helper.rb
@@ -2,6 +2,17 @@ module Kangaru
   module Initialisers
     module RSpec
       module RequestHelper
+        def stub_output(&block)
+          stdout  = $stdout
+          stderr  = $stderr
+          $stdout = File.open(File::NULL, "w")
+          $stderr = File.open(File::NULL, "w")
+
+          block.call
+
+          $stdout = stdout
+          $stderr = stderr
+        end
       end
 
       if Object.const_defined?(:RSpec)
@@ -13,6 +24,10 @@ module Kangaru
           end
 
           config.include RequestHelper, type: :request
+
+          config.around(type: :request) do |spec|
+            stub_output { spec.run }
+          end
         end
       end
     end
