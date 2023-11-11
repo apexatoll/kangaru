438386c0f9ea5c9a0fb24645ccd7f2bfc6a0ed12
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-11 01:53:06 +0000

    Update Configurators.classes to also return application configurators

--- lib/kangaru/configurators.rb
+++ lib/kangaru/configurators.rb
@@ -1,14 +1,21 @@
 module Kangaru
   module Configurators
     def self.classes
-      load_classes(self)
+      load_classes(self) + load_classes(application_configurators)
+    end
+
+    def self.application_configurators
+      Kangaru.application&.const_get(:Configurators)
     end
 
     def self.load_classes(root)
+      return [] if root.nil?
+
       root.constants.map    { |const| root.const_get(const) }
                     .select { |const| const.is_a?(Class) }
     end
 
+    private_class_method :application_configurators
     private_class_method :load_classes
   end
 end
