41f3a1390980dd4527d6beac2702f536c24dc0bf
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-11 01:05:10 +0000

    Remove base Configurator filtering from Configurators.classes

--- lib/kangaru/configurators.rb
+++ lib/kangaru/configurators.rb
@@ -1,8 +1,5 @@
 module Kangaru
   module Configurators
-    # These are not set as accessors by Config instances as they are abstract.
-    BASE_CONFIGURATORS = [].freeze
-
     def self.classes
       load_classes(self)
     end
@@ -10,7 +7,6 @@ module Kangaru
     def self.load_classes(root)
       root.constants.map    { |const| root.const_get(const) }
                     .select { |const| const.is_a?(Class) }
-                    .reject { |const| BASE_CONFIGURATORS.include?(const) }
     end
 
     private_class_method :load_classes
