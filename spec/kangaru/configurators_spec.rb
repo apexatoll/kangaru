d0117c2bf5dce3031596b903b2415fcf369392b4
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-11 01:53:06 +0000

    Update Configurators.classes to also return application configurators

--- spec/kangaru/configurators_spec.rb
+++ spec/kangaru/configurators_spec.rb
@@ -1,36 +1,143 @@
 RSpec.describe Kangaru::Configurators do
-  describe ".classes" do
+  describe ".classes", :stub_application do
     subject(:classes) { described_class.classes }
 
-    shared_context :stub_configurators do |options|
-      let(:constants) { options[:with] }
-
+    shared_context :application_set do
       before do
-        allow(described_class).to receive(:constants).and_return(constants)
+        allow(application)
+          .to receive(:const_get)
+          .with(:Configurators)
+          .and_return(application_configurators)
 
-        constants.each do |constant|
-          stub_const "#{described_class}::#{constant}", Class.new
+        stub_const "Application::Configurators", application_configurators
+      end
+    end
+
+    shared_context :stub_kangaru_configurators do |constants|
+      before do
+        stub_configurators(constants:, namespace: described_class)
+      end
+    end
+
+    shared_context :stub_application_configurators do |constants|
+      before do
+        stub_configurators(constants:, namespace: Application::Configurators)
+      end
+    end
+
+    def stub_configurators(constants:, namespace:)
+      allow(namespace).to receive(:constants).and_return(constants)
+
+      constants.each do |constant|
+        stub_const "#{namespace}::#{constant}", Class.new
+      end
+    end
+
+    context "when Kangaru configurators are not defined" do
+      include_context :stub_kangaru_configurators, []
+
+      context "and application is not set" do
+        let(:application) { nil }
+
+        it "returns no configurator classes" do
+          expect(classes).to be_empty
+        end
+      end
+
+      context "and application is set" do
+        include_context :application_set
+
+        context "and application does not define Configurators module" do
+          let(:application_configurators) { nil }
+
+          it "returns no configurator classes" do
+            expect(classes).to be_empty
+          end
+        end
+
+        context "and application defines Configurators module" do
+          let(:application_configurators) { Module.new }
+
+          context "and application does not define configurators" do
+            include_context :stub_application_configurators, []
+
+            it "returns no configurator classes" do
+              expect(classes).to be_empty
+            end
+          end
+
+          context "and application defines configurators" do
+            include_context :stub_application_configurators,
+                            %i[FooConfigurator BarConfigurator]
+
+            it "returns the application configurator classes" do
+              expect(classes).to contain_exactly(
+                Application::Configurators::FooConfigurator,
+                Application::Configurators::BarConfigurator
+              )
+            end
+          end
         end
       end
     end
 
-    context "when no configurator classes are defined" do
-      include_context :stub_configurators, with: []
+    context "when Kangaru configurators are defined" do
+      include_context :stub_kangaru_configurators,
+                      %i[FooConfigurator BarConfigurator]
 
-      it "returns an empty array" do
-        expect(classes).to be_empty
+      context "and application is not set" do
+        let(:application) { nil }
+
+        it "returns the Kangaru configurator classes" do
+          expect(classes).to contain_exactly(
+            described_class::FooConfigurator,
+            described_class::BarConfigurator
+          )
+        end
       end
-    end
 
-    context "when configurator classes are defined" do
-      include_context :stub_configurators,
-                      with: %i[FooConfigurator BazConfigurator]
+      context "and application is set" do
+        include_context :application_set
 
-      it "returns the expected configurators from the current namespace" do
-        expect(classes).to contain_exactly(
-          described_class::FooConfigurator,
-          described_class::BazConfigurator
-        )
+        context "and application does not define Configurators module" do
+          let(:application_configurators) { nil }
+
+          it "returns the Kangaru configurator classes" do
+            expect(classes).to contain_exactly(
+              described_class::FooConfigurator,
+              described_class::BarConfigurator
+            )
+          end
+        end
+
+        context "and application defines Configurators module" do
+          let(:application_configurators) { Module.new }
+
+          context "and application does not define configurators" do
+            include_context :stub_application_configurators, []
+
+            it "returns the Kangaru configurator classes" do
+              expect(classes).to contain_exactly(
+                described_class::FooConfigurator,
+                described_class::BarConfigurator
+              )
+            end
+          end
+
+          context "and application defines configurators" do
+            include_context :stub_application_configurators,
+                            %i[FooConfigurator BarConfigurator]
+
+            it "returns Kangaru and application configurator classes" do
+              expect(classes).to contain_exactly(
+                described_class::FooConfigurator,
+                described_class::BarConfigurator,
+                Application::Configurators::FooConfigurator,
+                Application::Configurators::BarConfigurator
+              )
+            end
+          end
+        end
       end
     end
   end
