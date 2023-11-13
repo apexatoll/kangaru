6031cb159f1d05c5c3297f1443d3a8ee22484b43
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-13 23:30:08 +0000

    Change env argument to a keyword arg

--- spec/support/features/configuration_helper.rb
+++ spec/support/features/configuration_helper.rb
@@ -1,7 +1,7 @@
 module ConfigurationHelper
   def configure_block(env: nil)
     <<~RUBY
-      configure #{env && ":#{env}"} do |config|
+      configure #{env && "env: :#{env}"} do |config|
         #{yield}
       end
     RUBY
