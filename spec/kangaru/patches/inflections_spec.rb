6a5f4955ce33164ae3011ff86146a53c78e8cf31
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2023-11-15 18:20:07 +0000

    Refactor inflections spec

--- spec/kangaru/patches/inflections_spec.rb
+++ spec/kangaru/patches/inflections_spec.rb
@@ -26,8 +26,22 @@ RSpec.describe Kangaru::Patches::Inflections do
     describe "#to_class_name" do
       subject(:class_name) { string.to_class_name }
 
-      include_examples :delegates_to_inflector,
-                       inflector: Kangaru::Inflectors::ClassInflector
+      let(:inflector_class) { Kangaru::Inflectors::ClassInflector }
+
+      let(:inflector) { instance_double(inflector_class, inflect: "output") }
+
+      before do
+        allow(inflector_class).to receive(:new).and_return(inflector)
+      end
+
+      it "instantiates a Class inflector" do
+        class_name
+        expect(inflector_class).to have_received(:new).with(string).once
+      end
+
+      it "inflects and returns the output" do
+        expect(class_name).to eq(inflector.inflect)
+      end
     end
 
     describe "#to_constant_name" do
